368dbd8c46b82b434e80a5015a3f78f20506fa14
ebrehault <ebrehault@gmail.com>
2014-01-15 10:47:40 +0100

    use CRUD API

--- js/microcms.js
+++ js/microcms.js
@@ -1,27 +1,42 @@
 var microcms = {};
 
-microcms.login = function() {
-    OAuth.initialize('sYYCj64pggkfSav1Ycm3PpmEAwo');
-    OAuth.popup('github', function(error, result) {
-        if(result) {
-            console.log("Logged in");
-        }
-    });
+microcms.params = {
+  repository: 'ebrehault/microcms',
+  branch: 'master',
+  public_key: 'sYYCj64pggkfSav1Ycm3PpmEAwo'
 };
 
 microcms.edit = function() {
-    document.designMode = "on";
+  document.designMode = "on";
 };
 
 microcms.save = function() {
-    
-};
+  OAuth.initialize(microcms.params.public_key);
+  OAuth.popup('github', function(error, result) {
+    if (error) {
+      console.log(err); // do something with error
+      return;
+    }
 
-microcms.console = function(url) {
-    $.getJSON(url, {}, function(response) {
-        var meta = response.meta
-        var data = response.data
-        console.log(meta)
-        console.log(data)
-    });
+    var base_url = 'https://api.github.com/repos/'
+    + microcms.params.repository
+    + '/contents/';
+    result.get(base_url + 'index.html').done(
+      function(response) {
+        console.log(response);
+        var sha = response.sha;
+        console.log("FILE SHA: " + sha);
+        result.put({
+          url: base_url + 'index.html',
+          data: JSON.stringify({
+            "message": "updated via microcms",
+            "content": btoa($('html')[0].outerHTML),
+            "sha": sha,
+            "path": 'index.html'
+          })
+        }).done(function(response) {
+          console.log(response);
+        });
+      });
+  });
 };
